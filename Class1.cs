b851ec68519f866b2ae5c775033b738113c1f004
VedantSahadeo <sahadeovedant@gmail.com>
2021-07-21 10:28:18 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestVedant1
+{
+   public class Add
+    {
+        public int Addtion( int x, int y)
+        {
+            int sum = x + y;
+
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestVedant1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
